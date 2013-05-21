8e376cb89811224d3a8a1fe824cc6114c6696092
ka2er <personseb+git@gmail.com>
2013-05-21 23:30:03 +0200

    add some logic for HTTP get command

--- arduino_shutters_light_automation.ino
+++ arduino_shutters_light_automation.ino
@@ -1,37 +1,56 @@
 /*
+ for Xboard Rleay v3.0 [DFRobot]
+ 
+ we could send simple http commands :
+ 
+ /
+ /set
+ 
 
-adapted from
-DFRobot X-board V2 Sample Code
-  
+ adapted from
+ DFRobot X-board V2 Sample Code
+ 
  A simple web server with DHPC capbabilty.
-1)Get IP address from router automatically
-2)Show the value of the analog input pins
-  
+ 1)Get IP address from router automatically
+ 2)Show the value of the analog input pins
+ 
  created 28 Sep 2012
  by Ricky
  */
- 
+
 #include <SPI.h>
 #include <Ethernet.h>
+
+//#include "http_action.h"
+
 EthernetServer server(80);
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = { 
   0xDE, 0xCD, 0xAE, 0x0F, 0xFE, 0xED };
- 
+
 // Initialize the Ethernet server library
 // with the IP address and port you want to use 
 // (port 80 is default for HTTP):
- 
- 
+
+int ledPin = 13;                 // LED connected to digital pin 13
+
+
+enum actions {
+  A_UNDEF,
+  A_READ,
+  A_SET
+};
+
+
 void setup() {
- // Open serial communications and wait for port to open:
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-  ; // wait for serial port to connect. Needed for Leonardo only
+    ; // wait for serial port to connect. Needed for Leonardo only
   }
-  
- 
+
+
   // start the Ethernet connection:
   if (Ethernet.begin(mac) == 0) {
     Serial.println("Failed to configure Ethernet using DHCP");
@@ -47,82 +66,141 @@ void setup() {
     Serial.print("."); 
   }
   Serial.println();
- 
+
   // start the Ethernet connection and the server:
-  
+
   server.begin();
   Serial.print("server is at ");
   Serial.println(Ethernet.localIP());
+
+  int ledPin = 13;                 // LED connected to digital pin 13
+  digitalWrite(ledPin, HIGH);   // sets the LED on
+  Serial.println("Setup Done !");
+
 }
- 
- 
+
+
+void send_pin_data(EthernetClient client) {
+
+  // send a standard http response header
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: application/json");
+  client.println("Connnection: close");
+  client.println();
+  client.print("{");
+  // add a meta refresh tag, so the browser pulls again every 5 seconds:
+  // output the value of each analog input pin
+  for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+    int sensorReading = analogRead(analogChannel);
+    client.print("\"analog_input_");
+    client.print(analogChannel);
+    client.print("\":");
+    client.print(sensorReading);
+    client.print(",");      
+
+  }
+
+  // output the value of each digital input pin
+  for (int digitalChannel = 2; digitalChannel < 13; digitalChannel++) {
+    int sensorReading = digitalRead(digitalChannel);
+    if(digitalChannel!=7&&digitalChannel!=8&&digitalChannel!=10)
+    {
+      client.print("\"digital_input_");
+      client.print(digitalChannel);
+      client.print("\":");
+      client.print(sensorReading);
+      client.print(","); 
+    }
+    else if(digitalChannel == 10) {
+      continue;
+    }
+    else
+    {
+      client.print("\"relay_");
+      client.print(digitalChannel);
+      client.print("\":");
+      client.print(sensorReading);
+      client.print(","); 
+
+    }
+
+
+  }
+
+
+
+  client.print("\"nop\":0}");
+  //client.flush();
+}
+
 void loop() {
+
+
+
   // listen for incoming clients
   EthernetClient client = server.available();
   if (client) {
+
+ 
+
     Serial.println("new client");
+    String request = ""; // request line
+   actions action = A_UNDEF;
+
     // an http request ends with a blank line
     boolean currentLineIsBlank = true;
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
+        request += c;
         Serial.write(c);
+
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
         if (c == '\n' && currentLineIsBlank) {
-          // send a standard http response header
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: application/json");
-          client.println("Connnection: close");
-          client.println();
-          client.print("{");
-                    // add a meta refresh tag, so the browser pulls again every 5 seconds:
-          // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("\"analog_input_");
-            client.print(analogChannel);
-            client.print("\":");
-            client.print(sensorReading);
-            client.print(",");      
-          
+
+          switch(action) {
+
+          case A_SET:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: application/json");
+            client.println("Connnection: close");
+            client.println();
+            client.print("{\"error\":\"tell me more\"}");
+            break;
+          case A_READ:
+            send_pin_data(client);
+            break;
+          default:
+            break;          
           }
-           
-             // output the value of each digital input pin
-          for (int digitalChannel = 2; digitalChannel < 10; digitalChannel++) {
-            int sensorReading = digitalRead(digitalChannel);
-            if(digitalChannel!=7&&digitalChannel!=8)
-            {
-            client.print("\"digital_input_");
-            client.print(digitalChannel);
-            client.print("\":");
-            client.print(sensorReading);
-             client.print(","); 
-            }
-             
-            else
-            {
-             client.print("\"relay_");
-            client.print(digitalChannel);
-            client.print("\":");
-            client.print(sensorReading);
-             client.print(","); 
-               
-            }
-                 
-          
-          }
-           
-           
-           
-          client.print("\"nop\":0}");
-          //client.flush();
-          break;
+        break; // break while loop
         }
+        
+        
         if (c == '\n') {
           // you're starting a new line
           currentLineIsBlank = true;
+          if(request.startsWith("GET /")) {
+            // we've got all the request line...
+
+
+
+            String s_action = request.substring(4, request.lastIndexOf(" "));
+            s_action.toLowerCase();
+           
+            if(s_action == "/set") {
+              action = A_SET;
+            }
+            else {
+              action = A_READ;
+            }
+
+            Serial.println("request is "+s_action);
+
+          }
+          request = ""; //
         } 
         else if (c != '\r') {
           // you've gotten a character on the current line
@@ -137,3 +215,5 @@ void loop() {
     Serial.println("client disconnected");
   }
 }
+
+
